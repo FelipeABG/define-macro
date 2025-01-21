c974c13ba3b07b6eccebde83e0be2b33958298cb
felipe <felipe.baleche2@gmail.com>
2025-01-21 16:13:11 -0300

    simplification

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,14 @@
-use parsed::{Grammar, Separator};
+use parsed::Grammar;
 use proc_macro::TokenStream;
-use quote::quote;
 
 mod parsed;
 
 #[proc_macro]
 pub fn bnf(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as Grammar);
-
-    if let Err(e) = check_input(&ast) {
-        let error = e.to_compile_error();
-        return quote! {#error}.into();
-    }
-
-    quote! {#ast}.into()
+    TokenStream::new()
 }
 
 fn check_input(bnf: &Grammar) -> syn::Result<()> {
-    // Check if there is an empty rule.
-    if let Some(rule) = bnf.rules.iter().find(|r| r.fields.is_empty()) {
-        return Err(syn::Error::new(
-            rule.name.span(),
-            "Invalid Rule. Expected at least one symbol",
-        ));
-    }
-
-    //Check if the separators are all the same in a rule
-    for rule in bnf.rules.iter() {
-        let sep = &rule.fields[0].separator;
-        for field in rule.fields.iter() {
-            if field.separator != *sep {
-                return Err(syn::Error::new(
-                    field.name.span(),
-                    "Expected either '|' or  ',' in a rule, not both",
-                ));
-            }
-        }
-    }
-
-    //Check if the struct rules fields have types
-    for rule in bnf.rules.iter() {
-        let sep = &rule.fields[0].separator;
-        if let Separator::Comma = sep {
-            for field in rule.fields.iter() {
-                if let None = field.ty {
-                    let msg = "Comma separated rules must have types in its fields";
-                    return Err(syn::Error::new(field.name.span(), msg));
-                }
-            }
-        }
-    }
-
-    Ok(())
+    todo!()
 }
--- src/parsed.rs
+++ src/parsed.rs
@@ -1,170 +1,81 @@
-use proc_macro2::TokenStream as TokenStream2;
-use quote::{quote, ToTokens};
 use syn::{
+    ext::IdentExt,
+    parenthesized,
     parse::{Parse, ParseStream},
     punctuated::Punctuated,
-    Token,
+    Result, Token,
 };
 
 pub struct Grammar {
-    pub rules: Punctuated<Rule, Token![;]>,
+    rules: Punctuated<Rule, Token![;]>,
 }
 
 pub struct Rule {
+    pub kw: syn::Ident,
     pub name: syn::Ident,
-    pub _arrow: Token![->],
+    pub arrow: Token![->],
     pub fields: Vec<Field>,
 }
 
 pub struct Field {
     pub name: syn::Ident,
-    pub _paren: Option<syn::token::Paren>,
+    pub paren: Option<syn::token::Paren>,
     pub ty: Option<syn::Type>,
-    pub separator: Separator,
-}
-
-#[derive(PartialEq, Eq)]
-pub enum Separator {
-    Comma,
-    Pipe,
 }
 
 impl Parse for Grammar {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
+    fn parse(input: ParseStream) -> Result<Self> {
         let rules = Punctuated::parse_terminated(input)?;
         Ok(Self { rules })
     }
 }
 
-impl ToTokens for Grammar {
-    fn to_tokens(&self, tokens: &mut TokenStream2) {
-        let rules: Vec<&Rule> = self.rules.iter().collect();
-
-        let ts = quote! {
-            #(#rules)*
-        };
-
-        tokens.extend(ts);
-    }
-}
-
 impl Parse for Rule {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let kw = input.call(syn::Ident::parse_any)?;
         let name = input.parse()?;
         let arrow = input.parse()?;
         let mut fields = Vec::new();
 
-        while let Ok(field) = input.parse() {
-            fields.push(field);
+        while !input.peek(Token![;]) {
+            fields.push(input.parse()?);
         }
 
         Ok(Self {
+            kw,
             name,
-            _arrow: arrow,
+            arrow,
             fields,
         })
     }
 }
 
-impl ToTokens for Rule {
-    fn to_tokens(&self, tokens: &mut TokenStream2) {
-        let name = syn::Ident::new(
-            &format!("{}", capitalize(&self.name.to_string())),
-            self.name.span(),
-        );
-        let fields = &self.fields;
-
-        let ts = match &self.fields[0].separator {
-            Separator::Comma => {
-                quote! {
-                    struct #name {
-                        #(#fields)*
-                    }
-                }
-            }
-            Separator::Pipe => {
-                quote! {
-                    enum #name {
-                        #(#fields)*
-                    }
-                }
-            }
-        };
-
-        tokens.extend(ts);
-    }
-}
-
 impl Parse for Field {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
+    fn parse(input: ParseStream) -> Result<Self> {
         let name = input.parse()?;
         let (paren, ty) = parse_type(input)?;
-        let separator = input.parse()?;
-        Ok(Self {
-            name,
-            _paren: paren,
-            ty,
-            separator,
-        })
+        parse_separator(input)?;
+        Ok(Self { name, paren, ty })
     }
 }
 
-impl ToTokens for Field {
-    fn to_tokens(&self, tokens: &mut TokenStream2) {
-        let ts = match self.separator {
-            Separator::Comma => {
-                let name = &self.name;
-                let ty = &self.ty.clone().unwrap();
-                quote! {#name: #ty,}
-            }
-            Separator::Pipe => {
-                let name = &syn::Ident::new(
-                    &format!("{}", capitalize(&self.name.to_string())),
-                    self.name.span(),
-                );
-
-                match &self.ty {
-                    Some(ty) => {
-                        quote! {#name(#ty),}
-                    }
-                    None => quote! {#name,},
-                }
-            }
-        };
-
-        tokens.extend(ts);
+fn parse_separator(input: ParseStream) -> Result<()> {
+    if input.peek(Token![,]) {
+        let _: Token![,] = input.parse()?;
+    } else if input.peek(Token![|]) {
+        let _: Token![|] = input.parse()?;
     }
+
+    Ok(())
 }
 
-fn capitalize(s: &str) -> String {
-    let mut c = s.chars();
-    match c.next() {
-        None => String::new(),
-        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
-    }
-}
-
-fn parse_type(input: ParseStream) -> syn::Result<(Option<syn::token::Paren>, Option<syn::Type>)> {
+fn parse_type(input: ParseStream) -> Result<(Option<syn::token::Paren>, Option<syn::Type>)> {
     if input.peek(syn::token::Paren) {
         let content;
-        let paren = syn::parenthesized!(content in input);
+        let paren = parenthesized!(content in input);
         let ty = content.parse()?;
         Ok((Some(paren), Some(ty)))
     } else {
         Ok((None, None))
-    }
-}
-
-impl Parse for Separator {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        if input.peek(Token![,]) {
-            input.parse::<Token![,]>()?;
-            Ok(Separator::Comma)
-        } else if input.peek(Token![|]) {
-            input.parse::<Token![|]>()?;
-            Ok(Separator::Pipe)
-        } else {
-            Err(input.error("Expected '|' or ','"))
-        }
     }
 }
