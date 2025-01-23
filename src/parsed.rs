0b6fb492d5f8096a9dae83a49eaaffe481cd8262
Felipe Augusto <felipe.baleche2@gmail.com>
2025-01-23 09:48:15 -0300

    added debugging to types

--- src/parsed.rs
+++ src/parsed.rs
@@ -41,7 +41,7 @@ impl ToTokens for Rule {
         );
         let fields = format_fields(self);
         let ts = quote! {
-            #[derive(Clone)]
+            #[derive(Clone, Debug)]
             pub #kw #name {
                 #(#fields)*
             }
